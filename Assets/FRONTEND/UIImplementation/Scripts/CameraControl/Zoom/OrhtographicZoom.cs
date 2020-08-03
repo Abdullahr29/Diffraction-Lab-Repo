04148e4dc637a0d8d48b87ad4a9786184eb30fb9
cgb18 <cgb18@ic.ac.uk>
2020-08-03 14:37:08 +0200

    Revert "intens"
    This reverts commit 2f1fb20a1b956f5bc3a81a19b3a89196d4df01d8.

---
+++ Assets/FRONTEND/UIImplementation/Scripts/CameraControl/Zoom/OrhtographicZoom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrhtographicZoom : IZoom
+{
+    public OrhtographicZoom(Camera cam, float startingZoom)
+    {
+        cam.orthographicSize = startingZoom;
+    }
+
+    public void ZoomIn(Camera cam, float delta, float nearZoomLimit)
+    {
+        if (cam.orthographicSize == nearZoomLimit) return;
+        cam.orthographicSize = Mathf.Max(cam.orthographicSize - delta, nearZoomLimit);
+    }
+
+    public void ZoomOut(Camera cam, float delta, float farZoomLimit)
+    {
+        if (cam.orthographicSize == farZoomLimit) return;
+        cam.orthographicSize = Mathf.Min(cam.orthographicSize - delta, farZoomLimit);
+    }
+
+    public void ZoomInMax(Camera cam, float nearZoomLimit)
+    {
+        cam.orthographicSize = nearZoomLimit;
+    }
+
+    public void ZoomOutMax(Camera cam, float farZoomLimit)
+    {
+        cam.orthographicSize = farZoomLimit;
+    }
+}
